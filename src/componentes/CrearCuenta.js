bb689a372ff06a9d957a53b9db8b8e3aabb00795
JhonnyCamilo <jhonnybeltran57@gmail.com>
2022-11-17 19:39:41 -0500

    primer commit

---
+++ src/componentes/CrearCuenta.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const CrearCuenta = () => {
+    return (
+        <div>
+        
+        <h1>Crear Cuenta</h1>
+        <h2>Ingrese los datos del usuario</h2>
+        
+        <input placeholder='Nombre' />
+        <input placeholder='Email' />
+        <input placeholder='Password' />
+        <input placeholder='confirmar' />
+       
+        <button>Crear Cuenta </button>
+        <Link to={"/"}>Regresar</Link>
+      </div>
+    )
+
+}
+
+export default CrearCuenta;
